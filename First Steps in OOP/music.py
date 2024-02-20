18d8e353cad55368ef02a6f0f33ebedd99d23dfb
MartinDch24 <martindoychev2007@gmail.com>
2024-02-20 20:39:21 +0200

    First OOP lecture

---
+++ First Steps in OOP/music.py
@@ -0,0 +1,11 @@
+class Music:
+    def __init__(self, title, artist, lyrics):
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+
+    def print_info(self):
+        return f'This is "{self.title}" from "{self.lyrics}"'
+
+    def play(self):
+        return self.lyrics
